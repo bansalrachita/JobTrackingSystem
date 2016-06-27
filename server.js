72cf4c79420b373ccefd73aff6e46f1d06befe94
rachita <bansal.r@husky.neu.edu>
2016-06-27 15:53:20 -0400

    server js

--- server.js
+++ server.js
@@ -12,25 +12,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(cookieParser());
 app.use(session({ secret: process.env.SESSION_SECRET }));
-
-// require ("./test/app.js")(app);`
-
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-var project = require("./project/app.js");
-project(app);
-
-// configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
-
-// require ("./test/app.js")(app);
-
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-// var assignment = require("./assignment/app.js");
-// assignment(app);
 
 var project = require("./project/app.js");
 project(app);
