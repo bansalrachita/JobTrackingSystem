876bd0defab0b41b3bfc8498194b6b188618e7c8
rachita <bansal.r@husky.neu.edu>
2016-06-27 15:45:54 -0400

    server js

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
 
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
+
+// require ("./test/app.js")(app);`
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+var project = require("./project/app.js");
+project(app);
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
